77af070751d03edb3d326f027cc93615785bfe38
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2019-02-28 19:15:45 -0500

    fixed bubblesort

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+/**
+ * BubbleSort
+ */
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                // swap if before is bigger than after
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
