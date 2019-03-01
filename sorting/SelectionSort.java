96dd9dcfb41de7ec862d44d913e592d8687321ac
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2019-02-28 20:07:01 -0500

    implemented selection sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+/**
+ * SelectionSort
+ */
+public class SelectionSort {
+
+    public static void sort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            // set index to sorted part of the array
+            int minIndex = i;
+            // loop over unsorted part of array
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+            // place max in its place
+            int tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+
+        sort(arr);
+
+        for (int num : arr) {
+            System.out.print(num);
+        }
+    }
+}
