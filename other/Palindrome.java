55c388b74081671a7afb944c83d9573440f089a5
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2019-03-13 10:49:35 -0400

    implemented palindrome

---
+++ other/Palindrome.java
@@ -0,0 +1,14 @@
+/**
+ * Palindrome
+ */
+public class Palindrome {
+
+    public static boolean isPalindrome(String word) {
+        int n = word.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (word.charAt(i) != word.charAt(n - i - 1))
+                return false;
+        }
+        return true;
+    }
+}
